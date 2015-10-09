5f13567c33ad508969086de0f2af32eb57dc3986
Chae Cramb <chaecramb@gmail.com>
2015-10-09 11:16:55 +0100

    create data array and hash

---
+++ warehouse_picker.rb
@@ -0,0 +1,36 @@
+bays = %w(a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10)
+
+inventory = {
+bath_fizzers: b7,
+blouse: a3,
+bookmark: a7,
+candy_wrapper: c8,
+chalk: c3,
+cookie_jar: b10,
+deodorant: b9,
+drill_press: c2,
+face_wash: c6,
+glow_stick: a9,
+hanger: a4,
+leg_warmers: c10,
+model_car: a8,
+nail_filer: b5,
+needle: a1,
+paint_brush: c7,
+photo_album: b4,
+picture_frame: b3,
+rubber_band: a10,
+rubber_duck: a5,
+rusty_nail: c1,
+sharpie: b2,
+shoe_lace: c9,
+shovel: a6,
+stop_sign: a2,
+thermometer: c5,
+tire_swing: b1,
+tissue_box: b8,
+tooth_paste: b6,
+word_search: c4
+}
+
+
