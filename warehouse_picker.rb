c2e3d653fdae6237265f6c6b551b2611b380c26c
Chae Cramb <chaecramb@gmail.com>
2015-10-09 11:39:52 +0100

    list items in bays

--- warehouse_picker.rb
+++ warehouse_picker.rb
@@ -1,36 +1,45 @@
 bays = %w(a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10)
 
 inventory = {
-bath_fizzers: b7,
-blouse: a3,
-bookmark: a7,
-candy_wrapper: c8,
-chalk: c3,
-cookie_jar: b10,
-deodorant: b9,
-drill_press: c2,
-face_wash: c6,
-glow_stick: a9,
-hanger: a4,
-leg_warmers: c10,
-model_car: a8,
-nail_filer: b5,
-needle: a1,
-paint_brush: c7,
-photo_album: b4,
-picture_frame: b3,
-rubber_band: a10,
-rubber_duck: a5,
-rusty_nail: c1,
-sharpie: b2,
-shoe_lace: c9,
-shovel: a6,
-stop_sign: a2,
-thermometer: c5,
-tire_swing: b1,
-tissue_box: b8,
-tooth_paste: b6,
-word_search: c4
+b7: "bath fizzers",
+a3: "blouse",
+a7: "bookmark",
+c8: "candy wrapper",
+c3: "chalk",
+b10: "cookie jar",
+b9: "deodorant",
+c2: "drill press",
+c6: "face wash",
+a9: "glow stick",
+a4: "hanger",
+c10: "leg warmers",
+a8: "model car",
+b5: "nail filer",
+a1: "needle",
+c7: "paint brush",
+b4: "photo album",
+b3: "picture frame",
+a10: "rubber band",
+a5: "rubber duck",
+c1: "rusty nail",
+b2: "sharpie",
+c9: "shoe lace",
+a6: "shovel",
+a2: "stop sign",
+c5: "thermometer",
+b8: "tissue box",
+b6: "tooth paste",
+c4: "word search" 
 }
+
+#Get list of bays
+user_input_bays = ["c1","a4","b2","c10","c6","b3","a3","a4","a5","a10"]
+
+#List items in those bays
+items = []
+user_input_bays.each { |bay| items << inventory[bay.to_sym] }
+puts items
+
+#Find farthest appart items
 
 
