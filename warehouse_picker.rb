154f393a245e5d23d60506fdb112aca4549d69da
Chae Cramb <chaecramb@gmail.com>
2015-10-10 13:05:31 +0100

    fix bug in mutliple loops of error checking

--- warehouse_picker.rb
+++ warehouse_picker.rb
@@ -1,8 +1,8 @@
 require "pry-byebug"
 
-bays = %w(a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10)
+@bays = %w(a10 a9 a8 a7 a6 a5 a4 a3 a2 a1 c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10)
 
-inventory = {
+@inventory = {
 b7: "bath fizzers",
 a3: "blouse",
 a7: "bookmark",
@@ -34,33 +34,118 @@ b6: "tooth paste",
 c4: "word search" 
 }
 
-#Get list of bays
-user_input_bays = ["c1","a4","b2","c10","c6","b3","a3","a4","a5","a10"]
-
-#List items in those bays
-items = []
-user_input_bays.each { |bay| items << inventory[bay.to_sym] }
-
-#Find furthest apart items
-first = user_input_bays.first
-last =  user_input_bays.last
-user_input_bays.each do |b|
-  first = b if bays.index(b) < bays.index(first)
-  last = b if bays.index(b) > bays.index(last)
+def get_input(a, b)
+  input = gets.chomp.downcase
+  if input == a || input == b
+    input
+  else
+    puts "Please enter \"#{a}\" or \"#{b}\""
+    get_input(a, b)
+  end
+end
+  
+def get_bays  
+  puts `clear`
+  puts "Please enter a list of bays with a space in between each bay e.g.:"
+  puts "a10 b3 c3 c3 a5"
+  puts
+  print "Enter bays: "
+  gets.chomp.split
 end
 
-#Find distance apart
-distance_apart = bays.index(last) - bays.index(first)
+def list_items_in_bays(bays)
+  items = []
+  bays.each { |bay| items << @inventory[bay.to_sym] }
+  items
+end
 
-#Get list of products
-user_input_products = ["tooth paste", "paint brush", "candy wrapper", "photo album", "bath fizzers", "word search", "rubber duck"]
+def furthest_apart(bays)
+  first = bays.first
+  last =  bays.last
+  bays.each do |b|
+    first = b if @bays.index(b) < @bays.index(first)
+    last = b if @bays.index(b) > @bays.index(last)
+  end
+  @bays.index(last) - @bays.index(first)
+end
 
-#Find bays that need to be visited
-bays_to_be_visited = user_input_products.collect { |p| inventory.key(p) }
+def check_for_invalid_products(products)
+  invalid_products = products.select { |p| p unless @inventory.value?(p) }
+  if invalid_products.any?
+    products -= invalid_products
+    puts "The following products are invalid: #{invalid_products.join(", ")}."
+    puts "Would you like to (r)e-enter these products, or (c)ontinue without them?"
+    add_more_products(products) if get_input("r", "c") == "r"
+  else
+    products
+  end
+end
 
-#Order bays to be visited
-puts bays_to_be_visited.sort { |a, b| bays.index(a.to_s) <=> bays.index(b.to_s) }
+def get_products
+  puts `clear`
+  puts "Available products:"
+  puts "#{@inventory.values.join(", ")}"
+  puts
+  puts "Please enter a list of products with each product on a new line e.g.:"
+  puts "rusty nail"
+  puts "sharpie"
+  puts "shoe lace"
+  puts "shovel"
+  puts "stop sign"
+  puts
+  puts "When you have finished your list press enter again."
+  puts "(Press enter to continue)"
+  gets
+  puts "Enter your list:"
+  products = []
+  until products.last == ""
+    products << gets.chomp.rstrip
+  end
+  products.pop
+  check_for_invalid_products(products)
+end
 
+def add_more_products(products)
+  puts `clear`
+  puts "Available products:"
+  puts "#{@inventory.values.join(", ")}"
+  puts
+  puts "Add extra products:"
+  until products.last == ""
+    products << gets.chomp.rstrip
+  end
+  products.pop
 
+  check_for_invalid_products(products)
+end
 
+def ordered_bays(products)
+  bays_to_be_visited = products.collect { |p| @inventory.key(p) }
+  ordered_path = bays_to_be_visited.sort { |a, b| @bays.index(a.to_s) <=> @bays.index(b.to_s) }
+end
 
+puts `clear`
+puts "Would you like to:"
+puts "(a) Find products by bay number."
+puts "(b) Find the most efficient route to collect your products."
+
+case get_input("a", "b")
+when "a" 
+  bays = get_bays
+  puts
+  puts "The products in bays #{bays[0..-2].join(", ")} and #{bays.last}, in order are:" 
+  puts list_items_in_bays(bays).join(", ")
+  puts
+  puts "The distance between the farthest apart items is: #{furthest_apart(bays)}"
+  puts
+  puts
+  puts
+when "b"
+  puts
+  products = get_products
+  puts "Colect your products in this order:"
+  puts ordered_bays(products)
+  puts
+  puts
+  puts
+end
